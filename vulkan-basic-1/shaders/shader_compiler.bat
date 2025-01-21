760f1f855083bf36d3739f118ec471188fd474b5
freekysachin <110700231+Official-liar@users.noreply.github.com>
2025-01-21 14:00:21 +0530

    gitignore changes

--- vulkan-basic-1/shaders/shader_compiler.bat
+++
@@ -1,2 +0,0 @@
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe shader.frag	-o fragment.spv
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe shader.vert	-o vertex.spv
--- vulkan-basic-1/shaders/test.bat
+++
@@ -1,2 +0,0 @@
-C:\VulkanSDK\1.3.296.0\Bin\spirv-dis.exe vertex.spv
-C:\VulkanSDK\1.3.296.0\Bin\spirv-dis.exe fragment.spv
